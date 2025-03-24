3104c62030a9108ca3e2cb6750f49fca8ee07a96
SW-64 <ettjddns6@gmail.com>
2025-03-24 21:46:44 +0900

    UPDATE : 쓰지 않는 주석 스타일 변경 및 유저 이메일 유효성검사 코드 추가

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -12,13 +12,15 @@ class EmailService {
   // 이메일 인증 코드 요청 API - 선생님 권한
   requestVerification = async (email) => {
     // 유저 데이터에 email을 조회한 후, 있으면 통과 없으면 에러 처리
-    // 추후 구현
+    const existedEmail = await this.authRepository.findUserByEmail(email);
+    if (!existedEmail) throw new NotFoundError('이메일이 존재하지 않습니다.');
 
+    /*
     // 구글 활용을 위해 아이디, 비밀번호 세팅
-    // const googleEmail = process.env.GOOGLE_EMAIL;
-    // const googlePassword = process.env.GOOGLE_PASSWORD;
-    // const emailService = process.env.EMAIL_SERVICE;
-
+    const googleEmail = process.env.GOOGLE_EMAIL;
+    const googlePassword = process.env.GOOGLE_PASSWORD;
+    const emailService = process.env.EMAIL_SERVICE;
+    */
     // 인증 코드 생성
     const cryptogram = generate4DigitRandom();
 
@@ -28,31 +30,33 @@ class EmailService {
     const value = cryptogram;
     redis.set(key, value, 'EX', 600); // 600초 = 10분 TTL
 
+    /*
     // createTransport = 메일 서버와 연결 설정, auth는 계정의 아이디 비번를 확인
-    // const transporter = nodemailer.createTransport({
-    //   service: emailService,
-    //   auth: {
-    //     user: googleEmail,
-    //     pass: googlePassword,
-    //   },
-    // });
+    const transporter = nodemailer.createTransport({
+      service: emailService,
+      auth: {
+        user: googleEmail,
+        pass: googlePassword,
+      },
+    });
 
     // mailOption = 보낼 메일의 내용 설정
-    // const mailOptions = {
-    //   from: googleEmail, //작성자
-    //   to: email, // 수신자
-    //   subject: 'Nodemailer Test', // 메일 제목
-    //   text: cryptogram, // 메일 내용
-    // };
+    const mailOptions = {
+      from: googleEmail, //작성자
+      to: email, // 수신자
+      subject: 'Nodemailer Test', // 메일 제목
+      text: cryptogram, // 메일 내용
+    };
 
     // sendMail = 실제로 메일을 보내는 함수
-    // transporter.sendMail(mailOptions, (error, info) => {
-    //   if (error) {
-    //     console.error(error);
-    //   } else {
-    //     console.log('Email Sent : ', info);
-    //   }
-    // });
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log('Email Sent : ', info);
+      }
+    });
+    */
 
     return cryptogram;
   };

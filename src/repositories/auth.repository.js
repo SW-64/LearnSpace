c5bee49bd2dd592dbefb82f003c2a5f0b1418bdc
gil-hyeon <lgh5498@naver.com>
2025-03-24 16:39:03 +0900

    FEAT: 회원가입, 로그인 API 생성

--- src/repositories/auth.repository.js
+++ src/repositories/auth.repository.js
@@ -1,3 +1,40 @@
-class AuthRepository{}
+import bcrypt from 'bcrypt';
+import { prisma } from '../utils/prisma.utils.js';
+import { authConstant } from '../constants/auth.constant.js';
 
-export default AuthRepository
+class AuthRepository {
+  create = async ({ email, name, role, subject, password }) => {
+    const hashedPassword = bcrypt.hashSync(
+      password,
+      authConstant.HASH_SALT_ROUNDS,
+    );
+
+    const data = await prisma.user.create({
+      data: {
+        email,
+        name,
+        role,
+        subject,
+        password: hashedPassword,
+      },
+    });
+
+    data.password = undefined;
+  };
+
+  findUserByEmail = async (email) => {
+    const user = await prisma.user.findUnique({ where: email });
+    return user;
+  };
+
+  findUserById = async (id) => {
+    const user = await prisma.user.findUnique({
+      where: { id },
+      omit: { password: true },
+    });
+
+    return user;
+  };
+}
+
+export default AuthRepository;
--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -3,10 +3,16 @@ import AuthRepository from '../repositories/auth.repository.js';
 import AuthService from '../services/auth.service.js';
 import AuthController from '../controllers/auth.controller.js';
 import { prisma } from './../utils/prisma.utils.js';
+import { signUpValidator } from '../middlewares/validators/sign-up-validator.middleware.js';
+import { signInValidator } from '../middlewares/validators/sign-in-validator.middleware.js';
 
 const authRouter = express.Router();
 const authRepository = new AuthRepository(prisma);
 const authService = new AuthService(authRepository);
 const authController = new AuthController(authService);
+
+authRouter.post('/sign-up', signUpValidator, authController.signUp);
+
+authRouter.post('/sign-in', signInValidator, authController.signIn);
 
 export { authRouter };
